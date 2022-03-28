edc04e43532c7ddf39885de801c28dea6e12c14d
bilalbentoumi <bilalbentoumi@gmail.com>
2022-03-28 11:51:24 +0100

    Add default page sections

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,43 @@
-if (CSS && 'paintWorklet' in CSS) {
-    CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners')
-}
+$(document).ready(function (){
+
+    if (CSS && 'paintWorklet' in CSS) {
+        CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners')
+    }
+
+    $(window).scroll(function (){
+
+        const scrollTop = $(window).scrollTop();
+
+        /* Header */
+        if (scrollTop >= ($('.header').height() / 2)) {
+            $('.header').addClass('active');
+        } else {
+            $('.header').removeClass('active');
+        }
+
+        let current = '';
+
+        $('.section').each(function (){
+
+            const sectionTop = $(this).offset().top;
+            const sectionHeight = $(this).height();
+
+            if (scrollTop >= sectionTop - (sectionHeight / 2)) {
+                current = $(this).attr("id");
+            }
+
+        });
+
+        $('.nav-link').removeClass('active');
+        $(`.nav-link[href="#${current}"]`).addClass('active');
+
+        if (window.history.replaceState) {
+            let url = window.location.origin + window.location.pathname + '#' + current;
+            window.history.replaceState({}, null, url);
+        }
+
+    });
+
+    $(window).scroll();
+
+});
